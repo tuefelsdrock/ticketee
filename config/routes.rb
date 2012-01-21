6fcf5a1b695f014367b6c040b6886f19cf0c6f27
tuefelsdrock <uwjh0808@uw.edu>
2012-01-21 15:44:04 -0800

    pg 58

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,7 @@ Ticketee::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
