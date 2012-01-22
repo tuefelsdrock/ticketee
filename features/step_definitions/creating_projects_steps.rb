0c6c420f99f2bd5828d07ad096979dd4af98e4c6
tuefelsdrock <uwjh0808@uw.edu>
2012-01-22 08:03:06 -0800

    capybara steps

--- features/step_definitions/creating_projects_steps.rb
+++ features/step_definitions/creating_projects_steps.rb
@@ -3,19 +3,19 @@ Given /^I am on the homepage$/ do
 end
 
 When /^I follow "([^"]*)"$/ do |arg1|
-  click_link('New Project')
+  click_link(arg1)
 end
 
 When /^I fill in "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+      fill_in arg1, :with => arg2
 end
 
 When /^I press "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  click_button(arg1)
 end
 
 Then /^I should see "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  page.has_content?(arg1)
 end
 
 
