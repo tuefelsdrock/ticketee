ba0addb5a60083c31bae46e2f0dc38eaa3a08213
tuefelsdrock <uwjh0808@uw.edu>
2012-01-23 10:32:09 -0800

    pg 76

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee").join(" - ") unless parts.empty?
+      end
+    end
+  end
+
+
 end
