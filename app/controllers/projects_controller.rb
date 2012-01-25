233afcad5d306ad7b25ba8a58653efab194333bb
tuefelsdrock <uwjh0808@uw.edu>
2012-01-24 18:57:03 -0800

    pg 97

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,9 @@
 class ProjectsController < ApplicationController
 
+  # call the find_project method to initialize for these actions...
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
+
+
   def index
     @projects = Project.all
   end
@@ -25,18 +29,15 @@ class ProjectsController < ApplicationController
 
 
   def show
-    @project = Project.find(params[:id])
   end
 
 
   def edit
-    @project = Project.find(params[:id])
   end
 
 
 
   def update
-    @project = Project.find(params[:id])
     if @project.update_attributes(params[:project])
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -48,11 +49,19 @@ class ProjectsController < ApplicationController
 
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     flash[:notice] = "Project has been deleted."
     redirect_to projects_path
   end
+
+  private
+    def find_project
+      @project = Project.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking" +
+      " for could not be found."
+      redirect_to projects_path
+    end
 
 
 end
