f8d43a53bbfc6dda55ccfa1108608c50d38cefcf
tuefelsdrock <uwjh0808@uw.edu>
2012-01-28 07:26:05 -0800

    pg 111

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,12 @@
 class Project < ActiveRecord::Base
 
-  has_many :tickets
+
+  #This operation is quick and is useful if you have a large number of tickets 
+  #that don’t have callbacks. 
+  #If you do have callbacks on Ticket for a destroy operation, then you should 
+  #use :dependent => :destroy.
+  has_many :tickets, :dependent => :delete_all
+
 
   validates :name, :presence => true
 end
