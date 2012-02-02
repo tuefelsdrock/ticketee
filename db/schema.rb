3b3e33d32934c022fda3e7da37a6984113208c0d
tuefelsdrock <uwjh0808@uw.edu>
2012-02-02 07:04:07 -0800

    pg 138

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120130202842) do
+ActiveRecord::Schema.define(:version => 20120202150341) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20120130202842) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
