b90d1d0046f448d93270851a8ad382284ee9d745
tuefelsdrock <uwjh0808@uw.edu>
2012-02-03 07:21:48 -0800

    pg 142

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+
   private
+
     def authorize_admin!
       authenticate_user!
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to root_path
+      unless current_user.admin?
+        flash[:alert] = "You must be an admin to do that."
+        redirect_to root_path
+      end
     end
-  end
 
 end
