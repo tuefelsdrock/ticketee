fa434cd4e5d15cd4f81667219d61a19533934b87
tuefelsdrock <uwjh0808@uw.edu>
2012-02-06 08:39:18 -0800

    pg 161

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+
 
   def index
     @users = User.all(:order => "email")
@@ -11,7 +13,7 @@ class Admin::UsersController < Admin::BaseController
 
   def create
     @user = User.new(params[:user])
-    @user.admin = params[:user][:admin] == "1"
+    set_admin #ensures that the user is made an admin directly before the save 
     if @user.save
       flash[:notice] = "User has been created."
       redirect_to admin_users_path
@@ -20,6 +22,46 @@ class Admin::UsersController < Admin::BaseController
       render :action => "new"
     end
   end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+ 
+  def update
+
+    #you need to delete the password parameters from
+    #params[:user] if they are blank. Otherwise, the application will attempt to update a
+    #user with a blank password, and Devise won’t allow that. 
+
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    set_admin #ensures that the user is made an admin directly before the save 
+
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been updated."
+      render :action => "edit"
+    end
+  end
+
+
+  private
+    def find_user
+     @user = User.find(params[:id])
+    end
+
+    def set_admin
+      @user.admin = params[:user][:admin] == "1"
+    end
 
 
 end
