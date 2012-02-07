e386b2c4781e988e74abbae48bb300dd32f262d6
tuefelsdrock <uwjh0808@uw.edu>
2012-02-06 16:23:03 -0800

    pg 185 restrict reading tickets to correct project scope

---
+++ factories/ticket_factory.rb
@@ -0,0 +1,7 @@
+Factory.define :ticket do |ticket|
+  ticket.title "A ticket"
+  ticket.description "A ticket, nothing more"
+  ticket.user { |u| u.association(:user) }
+  ticket.project { |p| p.association(:project) }
+end
+
