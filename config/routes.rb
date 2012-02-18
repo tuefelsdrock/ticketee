dc554ff03f8d2f5d06cede71185811fc9cadadd3
tuefelsdrock <uwjh0808@uw.edu>
2012-02-18 08:32:26 -0800

    pg 302

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,12 @@ Ticketee::Application.routes.draw do
 
   resources :tickets do
     resources :comments
+    # pg 301:
+    resources :tags do
+      member do
+        delete :remove
+      end
+    end
   end
 
   root :to => 'projects#index'
