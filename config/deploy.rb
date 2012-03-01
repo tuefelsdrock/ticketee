30cc50c20692c9454d9d39df984fd3b25b0520aa
tuefelsdrock <uwjh0808@uw.edu>
2012-03-01 12:57:18 -0800

    working capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,9 @@
-#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-#require 'rvm/capistrano'
+
+# the following is required for using a private rvm environment
+#
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_type, :user  # use the deploying account rvm
 
 require 'bundler/capistrano'
 
@@ -12,8 +16,8 @@ set :scm, :git
 role :web, "localhost"                          # Your HTTP server, Apache/etc
 role :app, "localhost"                          # This may be the same as your `Web` server
 role :db,  "localhost", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
 
+# I used the account target instead of ticketee.com for my install.
 set :user, "target"
 set :deploy_to, "/home/target/apps/#{application}"
 
@@ -21,6 +25,8 @@ set :use_sudo, false
 
 set :keep_releases, 5
 
+# complains about public/images etc, which are not in github, so disable the touch step:
+set :normalize_asset_timestamps, false
 
 namespace :deploy do
   task :start do ; end
